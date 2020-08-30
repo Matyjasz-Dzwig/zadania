b2c0e7ae00e56f7739a9c7dbd5ff25b459e4914b
Mateusz <62038477+Matyjasz-Dzwig@users.noreply.github.com>
2020-08-30 13:40:30 +0200

    30.08.2020

---
+++ Cory Althoff/07chapter-03.py
@@ -0,0 +1,5 @@
+list = ["Noc Żywych Trupów","Ekipa","Rodzina Soprano","Pamiętniki Wampirów"]
+
+for ele in list:
+    print(ele,list.index(ele))
+input()
---
+++ Cory Althoff/07chapter-04.py
@@ -0,0 +1,10 @@
+numbers = [2137,69,666,32]
+
+while True:
+    guess = input("Try to guess: ")
+    if guess == 'q':
+        break
+    if guess in numbers:
+        print("you guessed")
+    else:
+        print(guess, "is not in list")
